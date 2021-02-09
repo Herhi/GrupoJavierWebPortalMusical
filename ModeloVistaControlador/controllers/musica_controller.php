adc962d4da5536ba9019d5607d35e02f253c8039
Javier Gonzalez <71818412+Herhi@users.noreply.github.com>
2021-02-09 13:29:58 +0100

    Update musica_controller.php

--- ModeloVistaControlador/controllers/musica_controller.php
+++ ModeloVistaControlador/controllers/musica_controller.php
@@ -6,12 +6,26 @@ require_once("models/musica_model.php");
 if (!isset($_POST) || empty($_POST)) {
 $tracks=obtenerTracks();
 
-
-}else{
-	
-}
-
 //Llamada a la vista -- Intermediario entre vista y modelo !!!
 require_once("views/musica_view.phtml");
+	
+	
+ }else{ 
+
+    $track    = ($_POST['track']);
+    $track    = getTrackId($track);
+    $cantidad = ($_POST['cantidad']);
+    //$orderNumber = altaPedido($conn);
+    //insertarOrderDetails($conn,$orderNumber, $cantidad);
+
+
+  }
+}
+
+catch(PDOException $e) {  
+  	echo $stmt . "<br>" . $e->getMessage();
+}
+
+$conn = null;
 
 ?>
