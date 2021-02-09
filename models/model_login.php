1f03d779847db691c47d46d8686811247167b2d1
Daniel González Carretero <71818186+daniel-gonzalez-carretero@users.noreply.github.com>
2021-02-09 13:01:21 +0100

    ✏️ Añadidos Comentarios
    No vaya ser que luego me echen la bronca.

--- models/model_login.php
+++ models/model_login.php
@@ -3,7 +3,10 @@
 require_once("../db/db.php");
 
 function comprobarCredenciales($user, $clave) {
-
+	// Daniel González Carretrero
+	// Dado un $user y una $clave, se comprueba si existe algún usuario con esos credenciales
+	// Devuelve NULL si no existe o hay algún fallo, o el ID del Usuario (customerId) si existe
+	
 	global $conexion;
 
 	try {
@@ -22,6 +25,10 @@ function comprobarCredenciales($user, $clave) {
 }
 
 function crearCookieLogin($idUsuario) {
+	// Daniel González Carretero
+	// Dado un $idUsuario, se crea una cookie para mantener iniciada la sesión
+	// Se guarda el ID del Usuario (customerId)
+	
 	return setcookie("userId", $idUsuario, time() + (86400 * 10), "/"); // La cookie expira en 10 días
 }
 
