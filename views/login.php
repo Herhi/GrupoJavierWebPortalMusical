5e9f540b7227eebf2754649d08362bb8e0569183
Daniel González Carretero <71818186+daniel-gonzalez-carretero@users.noreply.github.com>
2021-02-11 09:18:05 +0100

    🐛 Corregido error al iniciar sesión
    Era necesario introducir los datos dos veces, o recargar la página una vez introducidos los datos por primera vez.
    Se soluciona redirigiendo al usuario de nuevo a este archivo (ver el archivo '/controllers/controller_login.php'

--- views/login.php
+++ views/login.php
@@ -3,6 +3,8 @@
 		// Redireccionar al usuario a otra página, ya se ha iniciado la sesión
 		echo "<p style='color: green;'>Ha iniciado sesión, redireccionando... (NO IMPREMENTADO AÚN, NO ES QUE NO FUNCIONE. ACORDAUS DE QUITAR ESTE MENSAJE PLS)</p><br>";
 		// header("Location: ");
+	} else if (isset($_GET) && isset($_GET["didWork"])) {
+		$iniciadoSesion = $_GET["didWork"] == "1"; // TRUE si es 1, FALSE si es 0. No se declara si no se ha enviado el formulario antes
 	}
 ?>
 
