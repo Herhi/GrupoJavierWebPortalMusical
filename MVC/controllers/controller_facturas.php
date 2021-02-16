dd44dae9b8f0d5435026238fa729ecb857185e40
raquelalcazar <71818474+raquelalcazar@users.noreply.github.com>
2021-02-16 11:59:09 +0100

    Create controller_facturas.php

---
+++ MVC/controllers/controller_facturas.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("../views/facturas.php");
+
+if(isset($_POST) && !empty($_POST)){
+
+	require_once("../models/model_facturas.php");
+
+	$customerId=$_COOKIE["userId"];
+	$fecha_ini=$_POST["fecha_ini"];
+	$fecha_fin=$_POST["fecha_fin"];
+
+	require_once("../views/funciones_view_facturas.php");
+
+	$facturas=obtenerFacturasFecha($customerId, $fecha_ini, $fecha_fin);
+
+	verFacturas($facturas);
+	//require_once("../views/facturas.php");
+}
+
+?>
